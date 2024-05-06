514a1419aee183276a0d0e9e46659d27c8d7ecc7
CLi <cli@CLis-MBP-2.lan>
2024-05-06 14:07:42 -0700

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,6 +4,7 @@
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
+    /*
     public static int nextNumber(int n) {
         if (n  == 128) {
             return 1;
@@ -22,6 +23,41 @@ public class Collatz {
             System.out.print(n + " ");
         }
         System.out.println();
+    }
+     */
+
+    /** nextNumber method implements the Collatz sequence, but doesn't include
+     * that Collatz sequence must terminate at 1. Will include that in
+     * main method, using a while loop. **/
+    public static int nextNumber(int n) {
+        if (n % 2 == 0) {
+            return n / 2;
+        } else {
+            return n * 3 + 1;
+        }
+    }
+
+    /** main method runs the nextNumber method starting from the number 5.
+     * Also includes the fact that Collatz sequence must terminate at 1
+     * using a while loop. **/
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print(n + " ");
+        while (n != 1) {               /* while loop, calls nextNumber method */
+            n = nextNumber(n);
+            System.out.print(n + " ");   /* syntax thing: using println will print your
+                                        results with each number on a separate line. While
+                                        just using print prints it all in one line. */
+    }
+      /*  System.out.println();       /* don't need this line, it doesn't actually do anything, the
+                                       while loop automatically prints out the final terminating number
+                                       of 1./
+       */
+
+
+
+
+
     }
 }
 
